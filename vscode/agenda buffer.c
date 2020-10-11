d644d1acf147d20d2acd6f8c1ae2aaa992ab21f7
Jonnhymoraes <moraesjonnhy@gmail.com>
2020-10-11 01:37:42 -0300

    buffer

--- vscode/agenda buffer.c
+++ vscode/agenda buffer.c
@@ -7,59 +7,66 @@ typedef struct agenda{
 //	int idade;
 }agenda;
 
-void insere(agenda *pAtual);
-void imprime(agenda *pAtual);
-void busca (char *nomeAux, agenda *pAtual, agenda *pInicio, int *qt, int *x);
+void insere (agenda *pAtual);
+void imprime (agenda *pAtual);
+void buscar (char *nomeAux, agenda *pAtual, agenda *pInicio, int *qt);
+void excluir (nomeAux, pAtual, pInicio, int *qt);
 
 int main(){
 
 	void *pBuffer;
 	int *qt, *opcao, *x;
 	char *nomeAux;
-	agenda *pInicio;
-	agenda *pAtual;
+	agenda *pInicio;	 // ponteiro inicial
+	agenda *pAtual;		// posição atual do ponteiro
 
-	pBuffer=malloc(sizeof(int)*3 + sizeof(char)*20);
-	qt=(int *)pBuffer;
-	*qt=0;
-	opcao=(int *)(qt+sizeof(int));
-	x=(int *)(opcao+sizeof(int));
-	*x=0;
-	nomeAux=(char *)(x+sizeof(int));
+	pBuffer = malloc(sizeof(int)*3 + sizeof(char)*20);    //colocando os ponteiros no buffer
+	qt = (int *)pBuffer;
+	*qt = 0;
+	opcao = (int *)(qt+sizeof(int));
+	x = (int *)(opcao+sizeof(int));
+	*x = 0;
+	nomeAux = (char *)(x+sizeof(int));
 
-	printf("\t\t***** AGENDA *****\n\n\t 1 - Inserir nome\n\t 2 - Listar nomes\n\t 3 - Buscar nome\n\t 0 - Sair\n");
-
+	
 	do{
+		printf("\t\t***** AGENDA *****\n\n\t 1 - Inserir nome\n\t 2 - Listar nomes\n\t 3 - Buscar nome\n\t 4 - Excluir nome\n\t 0 - Sair\n");
 		printf("\nDigite a Opcao: \n");
 		scanf("%d", opcao);
 
-		if(*opcao==1){
-			*qt=(*qt+1);
+		if(*opcao == 1){
+			*qt=(*qt + 1);
 			pBuffer = realloc(pBuffer, (sizeof(int)*3) + (sizeof(int)*20) + (sizeof(agenda) * (*qt)));
-			qt=(int *)pBuffer;
-			opcao=(int *)(qt+sizeof(int));
-			x=(int *)(opcao+sizeof(int));
-			*x=0;
-			nomeAux=(char *)(x+(sizeof(int)*3));
-			pInicio=(agenda *)(nomeAux+(sizeof(char)*20));
-			pAtual=(pInicio)+(sizeof(agenda)* ((*qt)-1));
+			qt = (int *)pBuffer;
+			opcao = (int *)(qt+sizeof(int));
+			x= (int *)(opcao+sizeof(int));
+			*x = 0;
+			nomeAux = (char *)(x+(sizeof(int)*3));
+			pInicio = (agenda *)(nomeAux+(sizeof(char)*20));
+			pAtual = (pInicio)+(sizeof(agenda)* ((*qt)-1));
 
 			insere(pAtual);
 		}
-		if(*opcao==2){
-			pAtual=pInicio;
+		if(*opcao == 2){
+			pAtual = pInicio;
 			for(*x=0;*x<*qt;(*x)++){
 				imprime(pAtual);
-				pAtual=pAtual+sizeof(agenda);
+				pAtual = pAtual+sizeof(agenda);
 			}
 		}
-		if(*opcao==3){
-			pInicio=(agenda *)((sizeof(int)*3)+(sizeof(char)*20)+(sizeof(agenda)*(*qt)));
+		if(*opcao == 3){
+			pInicio = (agenda *)((sizeof(int)*3)+(sizeof(char)*20)+(sizeof(agenda)*(*qt)));
 			printf("Digite o nome que deseja buscar:\n");
 			scanf("%s", nomeAux);
-			busca(nomeAux, pAtual, pInicio, qt, x);
+			buscar(nomeAux, pAtual, pInicio, int *qt);
 		}
-	}while(*opcao!=0);
+		if(*opcao == 4){
+			pInicio = (agenda *)((sizeof(int)*3)+(sizeof(char)*20)+(sizeof(agenda)*(*qt)));
+			printf("Digite o nome que deseja excluir:\n");
+			scanf("%s", nomeAux);
+			excluir(nomeAux, pAtual, pInicio, int *qt);
+		}
+	}while(*opcao != 0);
 
 	free(pBuffer);
 
@@ -79,18 +86,30 @@ void imprime(agenda *pAtual){
 	printf("\n******************\n");
 }
 
-void busca(char *nomeAux, agenda *pAtual, agenda *pInicio, int *qt, int *x){
+void buscar(char *nomeAux, agenda *pAtual, agenda *pInicio, int *qt){
 	pAtual = pInicio;
 	
-	for(*x=0;*x<*qt;(*x)++){
-		if(strcmp(pAtual->nome, nomeAux)==0){
+		while (*(qt) > 0){
+			if(strcmp(pAtual->nome, nomeAux) == 0){
 			printf("Encontrado!\n");
 			printf("Nome: %s\n", pAtual->nome);
 //			printf("Idade: %d\n", pAtual->idade);
 			break;
+			}
+		pAtual = pAtual+(sizeof(char)*20);
 		}
-		pAtual=pAtual+(sizeof(char)*20);
-	}
 	printf("Fim da Busca!\n");
 }
 
+void excluir (nomeAux, pAtual, pInicio, int *qt){
+	while(*(qt) > 0 ){
+		if(strstr(nomeAux, pAtual -> nome) != NULL){
+			pAtual = (pAtual + sizeof(agenda));
+			return pAtual;
+		}
+		pAtual += sizeof(agenda);
+		(*(qt))--;
+	}
+
+return NULL;
+}
